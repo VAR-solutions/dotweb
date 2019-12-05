67d5084686d48fa2efb0cde23b64aa6303a59b2e
antew7 <ananya.tewari.7@gmail.com>
2019-12-05 09:48:00 +0530

    UI Fixes

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,14 +1,16 @@
 <template>
   <v-layout row wrap style="height: 100%">
     <v-flex xs12 sm6 md6 lg6 class="side">
-        <v-card elevation="0" max-width="80%" class="mx-auto">
-      <h2 class="pb-1 robob" style="color:black">DOT, IIIT Vadodara</h2>
-      <p
-        class="pt-3 robo" 
-      >In publishing and graphic design, lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used before final copy is available, but it may also be used to temporarily replace copy in a process called greeking, which allows designers to consider form without the meaning of the text influencing the design.</p>
-    </v-card>
+      <v-card elevation="0" max-width="80%" class="mx-auto">
+        <h2 class="pb-1 robob" style="color:black">DOT, IIIT Vadodara</h2>
+        <p class="pt-3 robo">
+          Established in 2014, DOT is the Design club of Indian Institute of Information Technology, Vadodara.<br>
+          Dots are the most basic part of any creative style. If a person understands the way to connect the dots, designing becomes easier to interpret and imagine. The nine dots of the logo represent the same. All the alphabets can be written connecting the nine dots.
+          We as a team believe in working together and helping others to learn and grow. From designing for a fest to working for GDG, we have come a long way through and we try to raise the bar of our standards with each and every project.
+        </p>
+      </v-card>
     </v-flex>
-    
+
     <v-flex xs12 sm6 md6 lg6>
       <v-img style="height: 100%" aspect-ratio src="../assets/3.jpg"></v-img>
     </v-flex>
--- src/components/Members.vue
+++ src/components/Members.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container grid-list-sm align-content-space-between>
     <v-layout wrap>
-      <v-flex v-for="k in 8" :key="k" xs3 sm3 md3>
+      <v-flex v-for="k in 8" :key="k" xs3 sm3 md3 id="scroll-target" style="max-height: 530px" class="overflow-y-auto">
         <Member />
       </v-flex>
     </v-layout>
